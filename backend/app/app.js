d45fca82121d94bfdb058a7bc21ca98ac2ff6ea7
Justine Fernandez <jfernandez>
2023-11-04 07:37:29 +0800

    Added: metadata on the application

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,56 +1,89 @@
+const cors = require('cors');
 const express = require('express');
+const dotenv = require('dotenv'); 
 const app = express();
-const port = 4000;
+app.use(cors());
+app.use(express.json());
+
 const { Sequelize, Models, DataTypes } = require('sequelize');
-
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js with Hasura on Docker!');
-});
-
-
-// Request Handler
-app.post('/createItem', async (req, res) => {
-  const sequelize = new Sequelize('postgres://postgres:postgres@postgres:5432/postgres')
-  const Item = sequelize.define("items", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
-    created: {
+const sequelize = new Sequelize('postgres://postgres:postgres@postgres:5432/postgres')
+const Item = sequelize.define("items", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  created: {
+    type: DataTypes.DATE(6),
+    allowNull: true 
+  },
+  updated: {
       type: DataTypes.DATE(6),
       allowNull: true
-    },
-    updated: {
-        type: DataTypes.DATE(6),
-        allowNull: true
-    }
-  });
-  // try {
-  //   await sequelize.authenticate();
-  //   console.log('Connection has been established successfully.');
-  // } catch (error) {
-  //   console.error('Unable to connect to the database:', error);
-  // }
-
-
-  await sequelize.sync({ force: true });
-  const createdItem = await Item.create({ id: 399 });
-  console.log("Line 38", createdItem);
-  // success
-  return res.json({
-    item: 1
-  })
-  /*
-  // In case of errors:
-  return res.status(400).json({
-    message: "error happened"
-  })
-  */
-
-
-
+  }
+});
+const User = sequelize.define("Users", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  created_at: {
+    type: DataTypes.DATE(6),
+    allowNull: true
+  },
+  updated_at: {
+    type: DataTypes.DATE(6),
+    allowNull: true
+  },
+  email_address: {
+    type: DataTypes.STRING(255),
+  },
+  first_name: {
+    type: DataTypes.STRING(255),
+  },
+  last_name: {
+    type: DataTypes.STRING(255),
+  },
+  active_status: {
+    type: DataTypes.STRING(255),
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: "This is welcome page"
+  });
+});
+
+
+// Create User
+app.post('/createUser', async (req, res) => {
+  await sequelize.sync({ force: true });
+  const userCreated = await User.create(req.body);
+  console.log("User Created:", userCreated)
+  // success
+  return res.json(req.body)
+});
+  
+// Create Item Request Handler
+app.post('/createItem', async (req, res) => {
+  console.log(req.body)
+  await sequelize.sync({ force: true });
+  const itemCreated = await Item.create(req.body);
+  console.log("Item Created:", itemCreated)
+  // success
+  return res.json(req.body)
+});
+
+// // Get Item Request Handler
+// app.get('/getItems', async (req, res) => {
+//   console.log(req.body)
+//   const { id } = req.body
+//   await sequelize.sync({ force: true });
+//   const getItem = await Item.findAll();
+//   // success
+//   return res.json(getItem)
+// });
+
+app.listen(4000, () => {
+  console.log('Server listening on port 4000');
 });
